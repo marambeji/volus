cfe6f3b93f82257d396e77d5e620162f2421d339
Salim hizi <salim.hizi1234@gmail.com>
2026-08-26 00:24:20 +0100

    feat: enforce HR permissions on the countries controller

--- backend/src/modules/countries/countries.controller.ts
+++ backend/src/modules/countries/countries.controller.ts
@@ -10,12 +10,16 @@ import {
   Post,
   Put,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CountriesService } from './countries.service';
 import { CreateCountryDto } from './dto/create-country.dto';
 import { UpdateCountryDto } from './dto/update-country.dto';
 import { PaginationQueryDto } from '../../common/dto/pagination.dto';
+import { AdminGuard } from '../../common/guards/admin.guard';
+import { PermissionGuard } from '../../common/guards/permission.guard';
+import { RequireModule } from '../../common/decorators/require-module.decorator';
 
 @ApiTags('Countries')
 @Controller({ path: 'countries', version: '1' })
@@ -23,24 +27,32 @@ export class CountriesController {
   constructor(private readonly service: CountriesService) {}
 
   @Post()
+  @UseGuards(AdminGuard, PermissionGuard)
+  @RequireModule('countries', 'manage')
   @ApiOperation({ summary: 'Create a country' })
   create(@Body() dto: CreateCountryDto) {
     return this.service.create(dto);
   }
 
   @Get()
+  @UseGuards(PermissionGuard)
+  @RequireModule('countries', 'view')
   @ApiOperation({ summary: 'List countries with pagination and search' })
   findAll(@Query() query: PaginationQueryDto) {
     return this.service.findAll(query);
   }
 
   @Get(':id')
+  @UseGuards(PermissionGuard)
+  @RequireModule('countries', 'view')
   @ApiOperation({ summary: 'Get a country by ID' })
   findOne(@Param('id', ParseUUIDPipe) id: string) {
     return this.service.findOne(id);
   }
 
   @Put(':id')
+  @UseGuards(AdminGuard, PermissionGuard)
+  @RequireModule('countries', 'manage')
   @ApiOperation({ summary: 'Update a country' })
   update(
     @Param('id', ParseUUIDPipe) id: string,
@@ -50,6 +62,8 @@ export class CountriesController {
   }
 
   @Delete(':id')
+  @UseGuards(AdminGuard, PermissionGuard)
+  @RequireModule('countries', 'manage')
   @HttpCode(HttpStatus.NO_CONTENT)
   @ApiOperation({ summary: 'Soft-delete a country' })
   remove(@Param('id', ParseUUIDPipe) id: string) {
--- backend/src/modules/countries/countries.module.ts
+++ backend/src/modules/countries/countries.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Country } from './entities/country.entity';
 import { CountriesService } from './countries.service';
 import { CountriesController } from './countries.controller';
+import { HrPermissionsModule } from '../hr-permissions/hr-permissions.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Country])],
+  imports: [TypeOrmModule.forFeature([Country]), HrPermissionsModule],
   controllers: [CountriesController],
   providers: [CountriesService],
   exports: [CountriesService, TypeOrmModule],
